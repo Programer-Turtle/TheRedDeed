5140e471af5fcb45f48310cf18725c6e26d4effe
Turtle <125102455+Programer-Turtle@users.noreply.github.com>
2024-04-19 21:50:39 -0400

    Add files via upload

---
+++ Scripts/SlideShow.js
@@ -0,0 +1,21 @@
+let SlideImage = document.getElementById("SlideShowImage")
+
+let i = 1
+let Slidevalues = ["TheRedDeedLogo.jpg", "CanalDayParade.jpeg", "Flower.jpeg", "MowedLawn.jpeg", "MowedLawnWithDog.jpeg", "MowedLawnWithTractor.jpeg", "NightMowedLawn.jpeg", "RCPlane.jpeg", "StickCollection.jpeg", "Tractor.jpeg", "TractorInFeild.jpeg", "TractorTowTractor.jpeg"]
+
+function ChangeSlide(Number)
+{
+    i += Number
+    if(i > Slidevalues.length)
+    {
+        i = 1
+    }
+    else if(i< 1)
+    {
+        i = Slidevalues.length
+    }
+    SlideImage.src = "Images/" + Slidevalues[i - 1]
+    console.log("Images/" + Slidevalues[i - 1])
+}
+
+ChangeSlide(0)
